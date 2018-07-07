2db18af1e0b0c1fd22b649c82fa78079c129ec51
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-07-07 12:16:54 -0500

    fix 32 bit code

--- i386/test/hello.s
+++ i386/test/hello.s
@@ -1,35 +1,42 @@
-; .extern GetStdHandle
-; .extern WriteFile
-; .extern ExitProcess
-;
-; .section .rodata
-;
-; msg: .ascii "Hello World!\r\n"
-;
-; .equ msg_len, .-msg
-; .equ stdout_query, -11
-;
-; .section .data
-;
-; stdout: .long 0
-;
-; bytes_written: .long 0
-;
-; .section .text
-;
-; .global start
-;
-; start:
-;     mov $stdout_query, %rcx
-;     call GetStdHandle
-;     mov %rax, (stdout)
-;
-;     mov  (stdout), %rcx
-;     mov  $msg, %rdx
-;     mov  $msg_len, %r8
-;     mov  $bytes_written, %r9
-;     push $0
-;     call WriteFile
-;
-;     xor %rcx, %rcx
-;     call ExitProcess
+.extern _GetStdHandle@4
+.extern _WriteFile@20
+.extern _ExitProcess@4
+
+.section .rodata
+
+msg: .ascii "Hello World!\r\n"
+
+.equ msg_len, .-msg
+.equ stdout_query, -11
+
+.section .data
+
+stdout: .long 0
+
+bytes_written: .long 0
+
+.section .text
+
+.global start
+
+start:
+    sub $4, %esp
+    push $stdout_query
+    call _GetStdHandle@4
+    mov %eax, (stdout)
+    add $4, %esp
+
+    sub $20, %esp
+    push $0
+    push $bytes_written
+    push $msg_len
+    push $msg
+    push (stdout)
+    call _WriteFile@20
+    add $20, %esp
+
+    sub $4, %esp
+    push $0
+    call _ExitProcess@4
+    add $4, %esp
+    ret
